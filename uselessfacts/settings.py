798b1e98140066c412bf60766844b24ee1cc9772
Kaushal Sharma <skazi019@gmail.com>
2022-06-27 19:23:17 +0530

    added tailwind to the porject

--- uselessfacts/settings.py
+++ uselessfacts/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = "uselessfacts.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -155,7 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
--- v1/urls.py
+++ v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FactView, FactIDView
+from .views import FactView, FactIDView, HomepageView
 
 urlpatterns = [
+    path("", HomepageView.as_view(), name="home"),
     path("random-fact/", FactView.as_view()),
     path("fact/<int:id>/", FactIDView.as_view()),
 ]
