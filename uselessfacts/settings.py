92a60ac2450ab13afde5785fe32966b2bfbee1fc
Kaushal Sharma <skazi019@gmail.com>
2022-06-30 10:46:23 +0530

    setting change

--- uselessfacts/settings.py
+++ uselessfacts/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
